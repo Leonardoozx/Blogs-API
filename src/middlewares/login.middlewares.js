9833e67384416c63a7dd72b32ff3ecee8dce0fbf
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-09-23 22:13:07 -0300

    add login middleware

---
+++ src/middlewares/login.middlewares.js
@@ -0,0 +1,23 @@
+const userServices = require('../services/user');
+
+const verifiesIfEmailAndPassExists = ({ body }, res, next) => {
+  if (!body.email || !body.password) {
+    return res.status(400).json({
+      message: 'Some required fields are missing',
+    });
+  }
+  next();
+};
+
+const verifyEmailAndPass = async ({ body }, res, next) => {
+  const emailReg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const emailValidation = emailReg.test(body.email);
+  const validateUser = await userServices.findUserByEmail(body.email);
+  console.log(validateUser);
+  if (!validateUser || body.email === '' || body.password === '' || !emailValidation) {
+    return res.status(400).json({ message: 'Invalid fields' });
+  }
+  next();
+};
+
+module.exports = { verifiesIfEmailAndPassExists, verifyEmailAndPass };
