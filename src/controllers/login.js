b1f28468a484c355d12b17586cc7d9af76ff447c
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-09-24 14:13:29 -0300

    starting the development of a new controller

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -4,10 +4,11 @@ const jwt = require('jsonwebtoken');
 const secret = process.env.JWT_SECRET;
 
 const login = async ({ body }, res) => {
-    const jwtConfig = { algorithm: 'HS256' };
+  const jwtConfig = { algorithm: 'HS256' };
 
-    const token = jwt.sign({ data: { userId: body.email } }, secret, jwtConfig);
+  const token = jwt.sign({ data: { email: body.email } }, secret, jwtConfig);
 
-    res.status(200).json({ token });
+  res.status(200).json({ token });
 };
+
 module.exports = { login };
