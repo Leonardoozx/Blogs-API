fc7124df724c8a7be2fd62f810743a4ab81dd86a
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-09-23 21:07:28 -0300

    create categories tables

---
+++ src/migrations/20220923221244-create-category.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(
+      'categories',
+      {
+        id: {
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+          type: Sequelize.INTEGER,
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+      },
+    );
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('category');
+  },
+};
