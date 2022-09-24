5344755a25a3997450e7abff75e16c275fde63b8
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-09-23 22:12:31 -0300

    add new route

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-// ...
+const loginRoutes = require('./routes/login.routes');
 
 const app = express();
 
 app.use(express.json());
 
-// ...
+app.use('/login', loginRoutes);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
