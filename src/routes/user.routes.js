336c14e9328c38941a87ae9247ef0094837a2bc4
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-09-26 17:04:55 -0300

    .

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const userControllers = require('../controllers/user');
 const userMiddlewares = require('../middlewares/user.middleware');
+const verifyToken = require('../middlewares/verifyToken.middleware');
 
 const router = express.Router();
 
 router.post(
   '/',
+  verifyToken,
   userMiddlewares.verifyNameAndPass,
   userMiddlewares.verifyEmail,
   userControllers.insertUser,
