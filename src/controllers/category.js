0712ca05f5148e139c9b6a18cdcd80a4f365d808
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-09-26 19:26:57 -0300

    feat: validation for some fields when inserting category
    feat: show all categories controller

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -1,9 +1,16 @@
 const categoryServices = require('../services/category');
 
 const insertCategory = async ({ body }, res) => {
-  if (!body.name) { return res.status(400).json({ message: '"name" is required' }); }
+  if (!body.name) {
+    return res.status(400).json({ message: '"name" is required' });
+  }
   const newCategory = await categoryServices.insertCategory(body.name);
   res.status(201).json(newCategory);
 };
 
-module.exports = { insertCategory };
+const showAllCategories = async (_req, res) => {
+  const allCategories = await categoryServices.showAllCategories();
+  res.status(200).json(allCategories);
+};
+
+module.exports = { insertCategory, showAllCategories };
