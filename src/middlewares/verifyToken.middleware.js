04aa392da387746137d2e909dbacc2b9db19ed5b
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-09-26 17:04:44 -0300

    feat: a middleware for validating tokens

--- src/middlewares/verifyToken.middleware.js
+++ src/middlewares/verifyToken.middleware.js
@@ -1,5 +1,19 @@
-const verifyToken = (token) => {
-  console.log(token);
+require('dotenv/config');
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.JWT_SECRET;
+
+const verifyToken = async (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({ message: 'Token not found' });
+
+  try {
+    const decodedToken = jwt.verify(token, secret);
+    req.authorization = decodedToken.data.email;
+  } catch (e) {
+    return res.status(401).json({ message: `Expired or ${e}` });
+  }
+  next();
 };
 
 module.exports = verifyToken;
