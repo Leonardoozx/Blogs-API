f01389f9d744a63f8bf1f1da2cec3caad68bdbb0
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-09-26 17:53:30 -0300

    feat: route for showing only one user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,6 +8,8 @@ const router = express.Router();
 
 router.get('/', verifyToken, userControllers.showAllUsers);
 
+router.get('/:id', verifyToken, userControllers.showUserById);
+
 router.post(
   '/',
   userMiddlewares.verifyNameAndPass,
