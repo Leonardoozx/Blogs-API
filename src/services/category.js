aa7ab16bf6f3c2e271a3b30ed93f36f158826918
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-09-26 18:20:46 -0300

    feat: a service for inserting values into categories table

---
+++ src/services/category.js
@@ -0,0 +1,8 @@
+const { Category } = require('../models');
+
+const insertCategory = async (name) => {
+  const newCategory = await Category.create({ name });
+  return newCategory;
+};
+
+module.exports = { insertCategory };
