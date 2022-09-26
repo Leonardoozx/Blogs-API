98300191581e6725d29825575842087b91613043
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-09-26 19:27:21 -0300

    feat: blog posts model now is working

---
+++ src/models/BlogPost.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPosts = sequelize.define(
+    'BlogPost',
+    {
+      id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      userId: DataTypes.INTEGER,
+      published: DataTypes.DATE,
+      updated: DataTypes.DATE,
+    },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'blog_posts',
+    }
+  );
+  BlogPosts.associate = (models) => {
+    BlogPosts.belongsTo(models.User,
+      { foreignKey: 'userId', as: 'user' })
+  };
+  return BlogPosts;
+};
