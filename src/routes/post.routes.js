26df72ef045329aca415f9957abc347a931a4a0e
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-09-27 17:20:35 -0300

    feat: a route for updating posts

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -2,15 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
-const postControllers = require('../controllers/post');
 const verifyToken = require('../middlewares/verifyToken.middleware');
+const postControllers = require('../controllers/post');
 const postMiddlewares = require('../middlewares/post.middleware');
 
 router.use(verifyToken);
 
 router.post(
   '/',
-  postMiddlewares.verifyFields,
+  postMiddlewares.verifyInsertFields,
   postMiddlewares.verifyCategory,
   postControllers.insertPost,
 );
@@ -18,5 +18,12 @@ router.post(
 router.get('/', postControllers.showAllPosts);
 
 router.get('/:id', postControllers.showPostById);
+
+router.put(
+  '/:id',
+  postMiddlewares.authorizeUser,
+  postMiddlewares.verifyUpdateFields,
+  postControllers.updatePostById,
+);
 
 module.exports = router;
